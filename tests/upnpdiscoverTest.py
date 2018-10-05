9151a02008b873b30925de0b21533770724cc4fb
Ingo <ingo@hoeft-online.de>
2018-10-06 01:58:16 +0200

    upnpdiscover with search and listen, no unit tests

--- tests/upnpdiscoverTest.py
+++ tests/upnpdiscoverTest.py
@@ -1,28 +1,62 @@
 #!/usr/bin/env python3
 """Tests for upnpdiscover"""
 
-import unittest
-from unittest.mock import patch
+from unittest import TestCase, mock
+import time
 
-from upnp.upnpdiscover import SsdpClass
+import upnp
+#from upnp import upnpdiscover
+#from upnp.upnpdiscover import SSDPdatagram, Msearch, Msearch_verbose
+
+addr1 = ('192.168.49.1', 34731)
+datagram1 = (
+    "HTTP/1.1 200 OK\r\n"
+    "USN: uuid:f48c8d92-c3c0-6f29-0000-00004e74db48::upnp:rootdevice\r\n"
+    "CACHE-CONTROL: max-age=1800\r\n"
+    "EXT:\r\n"
+    "ST: upnp:rootdevice\r\n"
+    "LOCATION: http://192.168.10.107:41947/"
+    "upnp/dev/f48c8d92-c3c0-6f29-0000-00004e74db48/desc\r\n"
+    "SERVER: Linux/3.10.54 UPnP/1.0 Cling/2.0\r\n"
+    "\r\n")
+
+addr2 = ('192.168.10.3', 1900)
+datagram2 = (
+    "HTTP/1.1 200 OK\r\n"
+    "LOCATION: http://192.168.10.3:49000/fboxdesc.xml\r\n"
+    "SERVER: fritz-box UPnP/1.0 AVM FRITZ!Box 7490 113.07.01\r\n"
+    "CACHE-CONTROL: max-age=1800\r\n"
+    "EXT:\r\n"
+    "ST: upnp:rootdevice\r\n"
+    "USN: uuid:123402409-bccb-40e7-8e6c-3481C4FC71A9::upnp:rootdevice\r\n"
+    "\r\n")
+
+addr3 = ('192.168.10.3', 1900)
+datagram3 = (
+    "HTTP/1.1 200 OK\r\n"
+    "LOCATION: http://192.168.10.3:49000/fboxdesc.xml\r\n"
+    "SERVER: fritz-box UPnP/1.0 AVM FRITZ!Box 7490 113.07.01\r\n"
+    "CACHE-CONTROL: max-age=1800\r\n"
+    "EXT:\r\n"
+    "ST: upnp:rootdevice\r\n"
+    "USN: uuid:123402409-bccb-40e7-8e6c-3481C4FC71A9::upnp:rootdevice\r\n"
+    "\r\n")
 
 
-class upnpdiscoverTestCase(unittest.TestCase):
-    """Tests for upnpdiscover search and listen"""
+class upnpdiscoverTestCase(TestCase):
+    """This are tests for upnpdiscover search and listen.
 
-    @patch('upnp.upnpdiscover.socket.socket')     # Patch the class
-    def test_msearch(self, mock_socket):
-        """Test upnpdiscover but without online network socket (mocking)"""
-        oMock_socket = mock_socket.return_value   # we want the instance
+    The program opens an UDP socket and gets a data stream from the local
+    network. Because of its time consuming dynamic nature this cannot be
+    tested. So we have to patch methods 'open' and 'get' to have a constant
+    test environment.
+    """
+    @mock.patch('upnp.upnpdiscover.Msearch')
+    def test_msearch(self, mock_Msearch):
+        mock_Msearch.return_value.get.return_value = \
+            upnp.upnpdiscover.SSDPdatagram(addr1, datagram1.encode())
+        oMsearch = upnp.upnpdiscover.Msearch()
+        result = oMsearch.get()
+        self.assertIsInstance(result, upnp.upnpdiscover.SSDPdatagram)
 
-        # instantiate our service and set it up
-        oMock_socket.recvfrom.return_value = [b'method msearch\r\n',
-                                              '192.168.169.170']
-        oSsdp = SsdpClass()
-        oSsdp._unittest = True
-        oSsdp.msearch()
-        oMock_socket.settimeout.assert_called_with(2)
-
-        oMock_socket.recvfrom.return_value = [b'method listen\r\n',
-                                              '192.168.169.170']
-        oSsdp.listen()
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab autoindent nowrap
--- upnp/upnpdiscover.py
+++ upnp/upnpdiscover.py
@@ -4,29 +4,94 @@ references:
 [Multicast in Python](https://stackoverflow.com/q/603852/5014688)
 [Multicast programming](https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
 """
-
 import socket
 import struct
+from time import time
 import argparse
 
+VERSION = '0.1'
+SEARCH_TIMEOUT = 10   # we need this global for unit test
 
-class SsdpClass():
-    """Handle Simple Service Discovery Protocol"""
+
+class SSDPdatagram:
+    """This class represents a SSDP datagram received from a MSEARCH request.
+
+    It contains the raw datagram as received so we can always extract available
+    information. There are also some prepared often used parameter like a
+    timestamp when the datagram was fetched, network address from the sending
+    host, unique identifier of the device and some more.
+    """
+    timestamp = 0
+    ssdp_type = ''   # M-SEARCH, NOTIFY
+    addr = ''
+    # this are the needed fields with the same name from the replied datagram
+    usn = ''   # contains also a uuid
+    location = ''
+
+    _raw_data = None    # raw received datagram
+
+    def __init__(self, addr, raw_data):
+        """The constructor prepairs raw data representing a SSDP datagram."""
+        self.timestamp = time()
+        self.addr = str(addr).replace(" ", "")
+        self._raw_data = raw_data
+        _data = self.data
+        self.usn = _data.partition("USN: ")[2].partition("\r\n")[0]
+        self.location = _data.partition("LOCATION: ")[2].partition("\r\n")[0]
+        self.ssdp_type = _data.partition(" * ")[0]
+
+    def id(self):
+        """This returns the unique id of the datagram.
+
+        The id consists of two parts from the datagram:
+        - the URN consisting a uuid
+        - the LOCATION that points to the SCPD xml file
+        """
+        return(self.usn + ' ' + self.location)
+
+    @property
+    def data(self):
+        """This returns the decoded raw data as string so it is printable."""
+        return(self._raw_data.decode())
+
+
+class Mcast:
+    """Common class for search and listen multicast packages"""
+    # timeout = 0   no data to get
+    # timeout > 0   get data, value may be used for control
+    timeout = 0
+    open_timestamp = 0
 
     _MCAST_GRP = '239.255.255.250'
     _MCAST_PORT = 1900
+    # If you increase the size of RECVBUF you have more time between
+    # 'open' the connection and 'get' its data before buffer overflow
+    _RECVBUF = 4096
 
-    _unittest = False
+    _sock = None
 
-    def msearch(self):
-        """Active search for UPnP devices on the local network"""
 
-        timeout = 2
-        msg = \
+class Msearch(Mcast):
+    """Class for active searching of devices"""
+
+    _devicelist = []
+    _oDatagram = None
+
+    def open(self):
+        """Initialize and open a connection and join to the multicast group
+
+        After 'open' the connection you should 'get' the data as soon as
+        possible to avoid buffer overflow
+        """
+        global SEARCH_TIMEOUT
+
+        self.open_timestamp = time()
+        self.timeout = SEARCH_TIMEOUT
+        _msg = \
             'M-SEARCH * HTTP/1.1\r\n' \
             'HOST:239.255.255.250:1900\r\n' \
             'ST:upnp:rootdevice\r\n' \
-            'MX:' + str(timeout) + '\r\n' \
+            'MX:' + str(self.timeout) + '\r\n' \
             'MAN:"ssdp:discover"\r\n' \
             '\r\n'
 
@@ -34,45 +99,110 @@ class SsdpClass():
         # to the upnp multicast address and port.
         # IP_MULTICAST_LOOP is enabled by default.
         # IP_MULTICAST_TTL is set to 1 by default.
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
-                             socket.IPPROTO_UDP)
-        sock.settimeout(timeout)
-        sock.sendto(msg.encode(), (self._MCAST_GRP, self._MCAST_PORT))
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                   socket.IPPROTO_UDP)
+        self._sock.settimeout(self.timeout)
+        self._sock.sendto(_msg.encode(), (self._MCAST_GRP, self._MCAST_PORT))
 
-        # print received data within the timeout
-        if self._unittest:
-            data, addr = sock.recvfrom(4096)
-            print(addr, data.decode(), end='')
-        else:
+    def _get_datagram(self):
+        """Get next SSDP datagram from multicast net within the timeout
+
+        This method returns a SSDPdatagram object that represents a received
+        SSDP record from an upnp root device on the local netwwork.
+        """
+        if self.timeout != 0:
             try:
-                while True:
-                    data, addr = sock.recvfrom(4096)
-                    print(addr, data.decode(), end='')
+                data, addr = self._sock.recvfrom(self._RECVBUF)
+                if len(data) >= self._RECVBUF:
+                    raise SystemExit("ERROR: receive buffer overflow")
+                self._oDatagram = SSDPdatagram(addr, data)
             except socket.timeout:
-                pass
+                self.timeout = 0
 
-    def listen(self):
-        """Passive listen for notifies from devices on the local network"""
+    def get(self):
+        """Process SSDP datagram object
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
-                             socket.IPPROTO_UDP)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        If we get replies with same USN (included uuid) and LOCATION we make
+        it unique.
+        """
+        while True:
+            self._get_datagram()
+            # ._get_datagram() waits until it has data received, so if we
+            # return from a timeout exception we have to check timeout
+            if self.timeout == 0:
+                return('')
+            device = self._oDatagram.id()
+            if device not in self._devicelist:
+                self._devicelist.append(device)
+                return(device + '\r\n')
+
+
+class Msearch_verbose(Msearch):
+    def get(self):   # overload get() from parent
+        Msearch.get(self)
+        #return(str(self._oDatagram.addr) + ' ' + self._oDatagram.data)
+
+
+class Listen(Mcast):
+    """Passive listen for notifies from devices on the local network
+
+    We are only listen to the upnp multicast group.
+    """
+    def open(self):
+        """Initialize and open a connection and join to the multicast group"""
+
+        self.open_timestamp = time()
+        self.timeout = -1
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                   socket.IPPROTO_UDP)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # use MCAST_GRP instead of '' to listen only to MCAST_GRP,
         # not all groups on MCAST_PORT
-        sock.bind(('', self._MCAST_PORT))
+        # self._sock.bind(('', self._MCAST_PORT))
+        self._sock.bind((self._MCAST_GRP, self._MCAST_PORT))
         mreq = struct.pack("4sl", socket.inet_aton(self._MCAST_GRP),
                            socket.INADDR_ANY)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        if self._unittest:
-            data, addr = sock.recvfrom(4096)
-            print(addr, data.decode(), end='')
-        else:
+        self._sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+                              mreq)
+
+    def _get_datagram(self):
+        """Listen to the next SSDP datagram on the local network"""
+        if self.timeout != 0:
             try:
-                while True:
-                    data, addr = sock.recvfrom(4096)
-                    print(addr, '\n' + data.decode(), end='')
+                data, addr = self._sock.recvfrom(self._RECVBUF)
+                if len(data) >= self._RECVBUF:
+                    raise SystemExit("ERROR: receive buffer overflow")
+                self._oDatagram = SSDPdatagram(addr, data)
             except KeyboardInterrupt:
-                pass
+                self.timeout = 0
+
+    def get(self):
+        """Listen for upnp datagrams on the local network."""
+        self._get_datagram()
+        if self.timeout == 0:
+            return('')
+        rel_time = self._oDatagram.timestamp - self.open_timestamp
+        rel_time = "{:09.4f}".format(rel_time)
+        device = self._oDatagram.id()
+        return(rel_time + ' ' + self._oDatagram.ssdp_type + ' ' +
+               self._oDatagram.addr + ' ' + device + '\r\n')
+
+
+class Listen_default(Msearch):
+    def get(self):   # overload get() from parent
+        Msearch.get(self)
+        return(str(self._oDatagram.addr) + ' ' + self._oDatagram.data)
+
+
+def print_all(oMcast):
+    """Search for upnp root devices on the local network.
+
+    This is polymorphic execution and the result depends on the object that is
+    given as parameter.
+    """
+    oMcast.open()
+    while oMcast.timeout != 0:
+        print(oMcast.get(), end='', flush=True)
 
 
 def main():
@@ -83,19 +213,26 @@ def main():
                        help="passive listen for notifies from devices")
     group.add_argument("-s", "--search", action="store_true",
                        help="active search for UPnP devices")
+    group.add_argument("-S", "--verbose_search", action="store_true",
+                       help="active search for UPnP devices")
     group.add_argument("-v", "--version", action="store_true",
                        help="show program version")
     args = parser.parse_args()
-    o_ssdp = SsdpClass()
     if args.search:
-        o_ssdp.msearch()
+        print_all(Msearch())
+    elif args.verbose_search:
+        print_all(Msearch_verbose())
     elif args.listen:
-        o_ssdp.listen()
+        print_all(Listen())
     elif args.version:
-        print("version 0.0")
+        print("version 0.1")
+    elif args.verbose:
+        print("")
     else:
-        o_ssdp.msearch()
+        msearch()
 
 
 if __name__ == '__main__':
     main()
+
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab autoindent nowrap
